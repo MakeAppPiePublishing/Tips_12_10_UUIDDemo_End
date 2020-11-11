97257a7f9733bd510b9d0e30d0250387beb5c654
Steven Lipton <steve@makeapppie.com>
2020-11-11 12:29:33 -0600

    Initial Commit

---
+++ UUIDDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UUIDDemo
+//
+//  Created by Steven Lipton on 11/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UUIDDemo/UUIDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UUIDDemoApp.swift
+//  UUIDDemo
+//
+//  Created by Steven Lipton on 11/11/20.
+//
+
+import SwiftUI
+
+@main
+struct UUIDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
