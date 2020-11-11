2d7a0509d0f3f70c54894bcb72fbc410f691164d
Steven Lipton <steve@makeapppie.com>
2020-11-11 12:44:10 -0600

    ContentView Changes

--- UUIDDemo/ContentView.swift
+++ UUIDDemo/ContentView.swift
@@ -2,20 +2,47 @@
 //  ContentView.swift
 //  UUIDDemo
 //
-//  Created by Steven Lipton on 11/11/20.
+//  Created by Steven Lipton on 9/27/20.
 //
+// An exercise file for iOS Development Tips Weekly
+// A weekly course on LinkedIn Learning for iOS developers
+//  Season 12 (Q4 2020) video 10
+//  by Steven Lipton (C)2020, All rights reserved
+// Learn more at https://linkedin-learning.pxf.io/YxZgj
+//This Week:  Learn about UUID and one handy place to use it.
+//  For more code, go to http://bit.ly/AppPieGithub
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct Food:Identifiable{
+    var id = UUID()
+    var name:String
+}
+
+struct FavoriteFoods{
+    static var list = [
+        Food(name:"Pizza"),
+        Food(name:"Pasta"),
+        Food(name:"Bagels"),
+        Food(name:"Doughnuts"),
+        Food(name:"Pizza")
+    ]
+}
+
+struct ListDataView:View{
+    var foods = FavoriteFoods.list
+    var body: some View{
+        VStack{
+            Text("My Favorite Foods")
+            List(foods){item in
+                Label(item.name, systemImage: "hand.thumbsup.fill")
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ListDataView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ListDataView()
     }
 }
